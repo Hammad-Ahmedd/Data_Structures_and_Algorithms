a0712bec33d03ac77792d039c70972343260b6ca
Hammad-Ahmed-2005 <clashofclancoc5000@gmail.com>
2026-01-21 11:28:44 +0500

    Starting DSA

---
+++ Algorithms/LinearSearch/LinearSearch..cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[6] = {5, 3, 1, 4, 9, 10};
+    cout << linearSearch(arr, 6, 110);
+}
